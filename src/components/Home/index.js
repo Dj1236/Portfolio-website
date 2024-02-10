32f2e4b0012c41c23f702962bc41a3c7873cd9bc
Dj1236 <121332557+Dj1236@users.noreply.github.com>
2024-02-10 09:12:57 +0530

    Add files via upload

---
+++ src/components/Home/index.js
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logod.png'
+import Logo from './Logo'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['D', 'h', 'a', 'i', 'r', 'y', 'a']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+ useEffect(
+   () => {
+     // Check if setLetterClass and destroy are defined and accessible
+     console.log('setLetterClass:', setLetterClass)
+     
+
+     // Check if setTimeout is causing the issue
+     const timeoutId = setTimeout(() => {
+       setLetterClass('text-animate-hover')
+     }, 3000)
+
+     // Cleanup function to clear the timeout when the component unmounts
+     return () => clearTimeout(timeoutId)
+   },
+   [
+     /* Add dependencies if needed */
+   ]
+ )
+
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
